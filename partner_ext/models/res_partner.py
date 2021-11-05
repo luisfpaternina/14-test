4918d22aa7a69708a43df829d4c750e93a32e2e7
bitsluispaternina <89114780+lfpv1993@users.noreply.github.com>
2021-11-04 19:08:09 -0500

    Update res_partner.py

--- partner_ext/models/res_partner.py
+++ partner_ext/models/res_partner.py
@@ -214,6 +214,6 @@ class ResPartner(models.Model):
     def validate_email(self):
         for rec in self:
             if rec.email:
-                if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", rec.email):
+                if not re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", rec.email):
                     raise ValidationError(_(
                     'Invalid email format!'))
