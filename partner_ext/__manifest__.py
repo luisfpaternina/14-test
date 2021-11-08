3ad94c3e6ca7808a0cc31f6a712e47b94d27a594
bitsluispaternina <89114780+lfpv1993@users.noreply.github.com>
2021-11-08 18:30:30 -0500

    Update __manifest__.py

--- partner_ext/__manifest__.py
+++ partner_ext/__manifest__.py
@@ -25,6 +25,7 @@
     'views/res_partner_address.xml',
     'views/res_partner_address_cardinals.xml',
     'views/res_partner_city.xml',
+    'views/assets.xml',
                    
     ],
     'installable': True
