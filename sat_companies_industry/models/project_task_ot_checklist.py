22a7fa4d1d311f397192cd39b3d451ba3fe874a2
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-11 14:17:09 -0500

    Update project_task_ot_checklist.py

--- sat_companies_industry/models/project_task_ot_checklist.py
+++ sat_companies_industry/models/project_task_ot_checklist.py
@@ -19,19 +19,32 @@ class ProjectTaskOtChecklist(models.Model):
     accomplished = fields.Selection([
         ('yes','Yes'),
         ('Not','Not')],string="Accomplished", tracking=True)
-    code = fields.Char(string="Code")
-    check_1 = fields.Boolean(string="1ª")
-    check_2 = fields.Boolean(string="2ª")
-    check_3 = fields.Boolean(string="3ª")
-    check_4 = fields.Boolean(string="4ª")
-    check_5 = fields.Boolean(string="5ª")
-    check_6 = fields.Boolean(string="6ª")
-    check_7 = fields.Boolean(string="7ª")
-    check_8 = fields.Boolean(string="8ª")
-    check_9 = fields.Boolean(string="9ª")
-    check_10 = fields.Boolean(string="10ª")
-    check_11 = fields.Boolean(string="11ª")
-    check_12 = fields.Boolean(string="12ª")
+    code = fields.Char(
+        string="Code")
+    check_1 = fields.Boolean(
+        string="1ª")
+    check_2 = fields.Boolean(
+        string="2ª")
+    check_3 = fields.Boolean(
+        string="3ª")
+    check_4 = fields.Boolean(
+        string="4ª")
+    check_5 = fields.Boolean(
+        string="5ª")
+    check_6 = fields.Boolean(
+        string="6ª")
+    check_7 = fields.Boolean(
+        string="7ª")
+    check_8 = fields.Boolean(
+        string="8ª")
+    check_9 = fields.Boolean(
+        string="9ª")
+    check_10 = fields.Boolean(
+        string="10ª")
+    check_11 = fields.Boolean(
+        string="11ª")
+    check_12 = fields.Boolean(
+        string="12ª")
     line_number = fields.Char(
         string="N° line",
         readonly=True,
