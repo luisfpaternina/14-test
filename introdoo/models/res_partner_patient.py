f7125b455c09dabad6baf2383723836601306f98
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-13 12:06:42 -0500

    add

--- introdoo/models/res_partner_patient.py
+++ introdoo/models/res_partner_patient.py
@@ -17,8 +17,3 @@ class ResPartnerPatient(models.Model):
         string="Active",
         tracking=True,
         default=True)
-    
-
-    @api.onchange('name')
-    def _upper_name(self):        
-        self.name = self.name.upper() if self.name else False
