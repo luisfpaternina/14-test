a8efb7089a8ecc79b8e5fb85c185376050af884e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-14 14:09:38 -0500

    add

--- introdoo/controllers/main.py
+++ introdoo/controllers/main.py
@@ -5,11 +5,10 @@ from odoo.http import request
 class Patient(http.Controller):
     @http.route('/patient_webform', type="http", auth="public", website=True)
     def patient_webform(self, **kw):
-        countries = request.env['res.country'].sudo().search([])
+        countries_rec = request.env['res.country'].sudo().search([])
         return http.request.render('introdoo.create_patient', {})
 
     @http.route('/create/webpatient', type="http", auth="public", website=True)
     def create_webpatient(self, **kw):
         request.env['res.partner.patient'].sudo().create(kw)
-        countries = request.env['res.country'].sudo().search([])
         return request.render("introdoo.patient_thanks", {})
