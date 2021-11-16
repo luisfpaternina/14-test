2ede3e11065bc0e0e4433ee14050ad69c6a9fc64
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-16 09:02:31 -0500

    Update crm_lead.py

--- sat_companies_sale/models/crm_lead.py
+++ sat_companies_sale/models/crm_lead.py
@@ -26,10 +26,19 @@ class CrmLead(models.Model):
     is_medium_website = fields.Boolean(
         string="Website medium",
         compute="_compute_check_medium_id")
+    is_medium_email = fields.Boolean(
+        string="Email medium",
+        compute="_compute_check_email_medium_id")
 
 
     @api.depends('medium_id')
     def _compute_check_medium_id(self):
         for record in self:
             record.is_medium_website = True if record.medium_id and record.medium_id[0].name  == 'Website' else False
+
+
+    @api.depends('medium_id')
+    def _compute_check_email_medium_id(self):
+        for record in self:
+            record.is_medium_email = True if record.medium_id and record.medium_id[0].name  == 'Email' else False
     
