6ece4c8e4aaf7ecec501b7d9c1ad6d55fb97b658
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-16 11:29:52 -0500

    Update res_partner.py

--- sat_companies_partner/models/res_partner.py
+++ sat_companies_partner/models/res_partner.py
@@ -130,11 +130,8 @@ class ResPartner(models.Model):
     gadget_oca_ids = fields.Many2many(
         'product.template',
         string='Gadgets')
-    gadget_comun_ids = fields.Many2many(
-        'product.template',
-        string='Gadgets')
-    
 
+    
     _sql_constraints = [
         (
             'client_code_uniq',
