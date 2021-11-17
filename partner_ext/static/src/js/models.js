9366564a3a265b01c3adcdcd412884defa105775
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-17 18:53:18 -0500

    update

--- partner_ext/static/src/js/models.js
+++
@@ -1,8 +0,0 @@
-odoo.define('pos_partner_firstname.models', function (require) {
-    "use strict";
-
-  var models = require('point_of_sale.models');
-
-  models.load_fields("res.partner", ["first_name", "second_name"]);
-
-});
