288fafc6267d1fae77056ffc7d957c525af3831e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-22 16:15:45 -0500

    add

--- nybble_website/controllers/main.py
+++ nybble_website/controllers/main.py
@@ -1,10 +1,10 @@
 from odoo import http
 from odoo.http import request
 
-
 class Patient(http.Controller):
     @http.route('/patient_webform', type="http", auth="public", website=True)
     def patient_webform(self, **kw):
+        ticket_type_rec = request.env['helpdesk.ticket.type'].sudo().search([])
         return http.request.render('nybble_website.create_patient', {})
 
 
