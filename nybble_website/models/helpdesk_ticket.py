2c13c87319247c9d370a741aeead933a50fcfc23
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-22 08:22:03 -0500

    Update helpdesk_ticket.py

--- nybble_website/models/helpdesk_ticket.py
+++ nybble_website/models/helpdesk_ticket.py
@@ -2,3 +2,13 @@ from odoo import models, fields, api, _
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
+
+    priority = fields.Selection(
+    selection=[
+        ('0', 'Todos'),
+        ('1', 'Prioridad baja'),
+        ('2', 'Alta prioridad'),
+        ('3', 'Urgente'),
+        ('4', 'Bloqueante'),
+    ],
+)
