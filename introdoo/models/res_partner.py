8217ae2be1886e9defcba23d135baf0126923a25
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-11-25 16:09:25 -0500

    Update res_partner.py

--- introdoo/models/res_partner.py
+++ introdoo/models/res_partner.py
@@ -5,3 +5,10 @@ import re
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    @api.constrains('phone')
+    def validate_phone(self):
+        for rec in self:
+            if rec.phone:
+                if len(rec.phone) < 6 or re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.phone):
+                    raise ValidationError('El número de télefono no puede contener letras')
